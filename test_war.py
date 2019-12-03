46ac10aa4316639005de5a2195b33edee37cd674
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-02 22:19:03 -0800

    Add new card tests.

--- test_war.py
+++ test_war.py
@@ -2,8 +2,8 @@ import war
 
 def test_card():
     """Test cards"""
-    # value, suite, strig
-    card_variables = [
+    # value, suite, name.
+    variables = [
         (2, 0, '2 of spades'),
         (3, 1, '3 of hearts'),
         (10, 2, '10 of diamonds'),
@@ -11,5 +11,46 @@ def test_card():
         (13, 1, 'King of hearts'),
     ]
 
-    for value, suite, card_name in card_variables:
+    for value, suite, card_name in variables:
         assert war.Card(value, suite).__repr__() == card_name
+
+
+def test_card_gt():
+    "Test 'Greater Than' magic method."
+
+    variables = [
+        # Test value Greater than.
+        (3, 0, 2, 0, True),
+        (10, 0, 5, 0, True),
+        (2, 3, 8, 2, False),
+        (4, 0, 11, 1, False),
+        (4, 2, 1, 3, True),
+        # Test suite greater than.
+        (4, 5, 4, 3, True),
+        (10, 3, 10, 0, True),
+        (8, 1, 8, 2, False),
+        (12, 1, 12, 3, False),
+    ]
+
+    for x in variables:
+        assert (war.Card(x[0], x[1]) > war.Card(x[2], x[3])) == x[4]
+
+def test_card_lt():
+    "Test 'Less Than' magic method."
+
+    variables = [
+        # Test value Less Than.
+        (3, 0, 2, 0, False),
+        (10, 0, 5, 0, False),
+        (2, 3, 8, 2, True),
+        (4, 0, 11, 1, True),
+        (4, 2, 1, 3, False),
+        # Test suite Less Than.
+        (4, 5, 4, 3, False),
+        (10, 3, 10, 0, False),
+        (8, 1, 8, 2, True),
+        (12, 1, 12, 3, True),
+    ]
+
+    for x in variables:
+        assert (war.Card(x[0], x[1]) < war.Card(x[2], x[3])) == x[4]
