29547c28fd5657d9c6192b7d33da5b107c3d40b6
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-19 10:29:15 -0800

    [snake] Collect controller input to snake head.

--- snake/snake.cpp
+++ snake/snake.cpp
@@ -87,7 +87,21 @@ void Input()
 }
 void Logic()
 {
-	
+	switch (dir)
+	{
+		case LEFT:
+			x--;
+			break;
+		case RIGHT:
+			x++;
+			break;
+		case UP:
+			y++;
+			break;
+		case DOWN:
+			y--;
+			break;
+	}
 }
 
 int main()
