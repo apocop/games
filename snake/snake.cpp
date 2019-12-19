d448de8c63d5ec052ba682581acda64651e1143e
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-18 22:49:59 -0800

    [snake] Add basic structure.

--- snake/snake.cpp
+++ snake/snake.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
+bool gameOver;
 
+void Setup()
+{
+	gameOver = false;
+}
+void Draw()
+{
+	
+}
+void Input()
+{
+	
+}
+void Logic()
+{
+	
+}
 
 int main()
 {
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+	}
 	
 }
